440e0aed2e14c42d9ed9966043482d9f488fd614
SKupisz <szym-ku@wp.pl>
2020-01-28 15:54:15 +0100

    Added a short audio description of project

--- src/loading/js/subcomponents/about.jsx
+++ src/loading/js/subcomponents/about.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import Particles from "react-particles-js";
 import {Link} from "react-router-dom";
+import Speech from "react-speech";
 
 import logo2 from "../../../img/logo2.png";
 import ceo from "../../../img/ceo.webp";
 
+
 export default class About extends React.Component{
+    constructor(props){
+        super(props);
+        this.deltaDesc = "What is Delta? It's a cyphering system that will secure your text data like a dream. By the construction of our cypher, final cryptogram could be decrypted as even dozens of results. How we are making this that safe? By the cypher structure — during cyphering process, we are using many different mathematic transformations, like integrals, infinitesimal calculus and many others. However, the cyphering process is not fixed - we are still adding more and more calculus to that, becuase we believe this will make our cypher stronger.";
+        //this.deltaForRead = "What is Delta? It's a cyphering system that will secure your text data like a dream. By the construction of our cypher, final cryptogram could be decrypted as even dozens of results. How we are making this that safe? During cyphering process, we are using many different mathematic transformations, like integrals, infinitesimal calculus and many others. However, the cyphering process is not fixed, we are still adding more and more calculus to that, becuase we believe this will make our cypher stronger.";
+        this.deltaForRead = "Delta is an experimental cypher made by the antares group. We hope that this project can be a solution of many problems in cryptography or it would be just an interesting activity. If you want to contact us write on sgk-news@wp.pl";
+        this.speechRef = React.createRef();
+    }
     render(){
         return(
             <section className="about-containter">
                 <header className="about-mainHeader">About us</header>
                 <section className="describe delta-describe">
                     <img src={logo2} alt="" className = "delta-icon"/>
-                    <div className="describe-content">What is Delta? It's a cyphering system that will secure your text data like a dream. By the construction of our cypher, final cryptogram could be decrypted as even dozens of results. How we are making this that safe? By the cypher structure - during cyphering process, we are using many different mathematic transformations, like integrals, infinitesimal calculus and many others. However, the cyphering process is not fixed - we are still adding more and more calculus to that, becuase we believe this will make our cypher stronger.</div>
+                    <section className="speech-container">
+                    <Speech
+                    className = "button" textAsButton={true} voice="Google UK English Female" displayText="🔊" text = {this.deltaForRead}/></section>
+                    <div className="describe-content">{this.deltaDesc}</div>
                 </section>
                 <section className="describe stuff-describe">
                     <img src={ceo} alt="" className = "stuff-icon"/>
@@ -26,6 +38,7 @@ export default class About extends React.Component{
                         <button className="link-button">Cyphering panel</button>
                     </Link>    
                 </section>
+                
                 <Particles ref = {this.particles} width = {"100%"} className = "particles-content"
                 params = {{ 
                     particles: {
